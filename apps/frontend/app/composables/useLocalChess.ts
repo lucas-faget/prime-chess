e3ad57a069985fdf345da81e7fcb0148e48041f3
lucas-faget <lucasfaget98@gmail.com>
2025-11-09 13:31:13 +0100

    feat(composables/useLocalChess): enable navigation through previous moves

--- apps/frontend/app/composables/useLocalChess.ts
+++ apps/frontend/app/composables/useLocalChess.ts
@@ -1,18 +1,31 @@
-import { chess, type Chess, type Chessboard, type Direction, type LegalMoves, type Move } from "@primechess/chess-lib";
+import {
+    chess,
+    type Chess,
+    type Chessboard,
+    type Direction,
+    type HistoryEntry,
+    type LegalMoves,
+    type Move,
+    type Squares,
+} from "@primechess/chess-lib";
 
 export function useLocalChess() {
     const game = ref<Chess>(chess.new());
+    let board = ref<Chessboard>(game.value.getChessboard());
+    let squares = ref<Squares>(board.value.getSquares());
 
     const rows = computed<string[]>(() => {
-        return playerInFrontIndex.value === 0 ? [...game.value.ranks].reverse() : game.value.ranks;
+        return playerInFrontIndex.value === 0 ? [...board.value.ranks].reverse() : board.value.ranks;
     });
 
     const columns = computed<string[]>(() => {
-        return playerInFrontIndex.value === 0 ? game.value.files : [...game.value.files].reverse();
+        return playerInFrontIndex.value === 0 ? board.value.files : [...board.value.files].reverse();
     });
 
-    const chessboard = ref<Chessboard>(game.value.getChessboard());
     const legalMoves = ref<LegalMoves>(game.value.getLegalMoves());
+    const history = ref<HistoryEntry[]>(game.value.getHistory());
+    const lastHalfmoveIndex = ref<number>(0);
+    const activeHalfmoveIndex = ref<number>(0);
     const playerInFrontIndex = ref<number>(0);
     const playerInFrontDirection = computed<Direction>(() => game.value.players[playerInFrontIndex.value]!.direction);
 
@@ -23,8 +36,11 @@ export function useLocalChess() {
     function tryMove(from: string, to: string): Move | null {
         const move = game.value.tryMove(from, to);
         if (move) {
-            chessboard.value = game.value.getChessboard();
+            board.value.carryOutMove(move);
+            squares.value = board.value.getSquares();
             legalMoves.value = game.value.getLegalMoves();
+            lastHalfmoveIndex.value++;
+            activeHalfmoveIndex.value++;
         }
         return move;
     }
@@ -33,11 +49,53 @@ export function useLocalChess() {
         playerInFrontIndex.value = (playerInFrontIndex.value + 1) % game.value.players.length;
     }
 
+    function goToMove(halfmoveIndex: number): void {
+        if (
+            halfmoveIndex !== activeHalfmoveIndex.value &&
+            halfmoveIndex > 0 &&
+            halfmoveIndex <= lastHalfmoveIndex.value
+        ) {
+            const fen: string | undefined = history.value[halfmoveIndex]?.fen;
+            if (fen) {
+                board.value.fill(fen);
+                squares.value = board.value.getSquares();
+                activeHalfmoveIndex.value = halfmoveIndex;
+            }
+        }
+    }
+
+    function goToFirstMove(): void {
+        if (activeHalfmoveIndex.value > 1) {
+            goToMove(1);
+        }
+    }
+
+    function goToPreviousMove(): void {
+        if (activeHalfmoveIndex.value > 1) {
+            goToMove(activeHalfmoveIndex.value - 1);
+        }
+    }
+
+    function goToNextMove(): void {
+        if (activeHalfmoveIndex.value < lastHalfmoveIndex.value) {
+            goToMove(activeHalfmoveIndex.value + 1);
+        }
+    }
+
+    function goToLastMove(): void {
+        if (activeHalfmoveIndex.value < lastHalfmoveIndex.value) {
+            goToMove(lastHalfmoveIndex.value);
+        }
+    }
+
     function cancelLastMove(): Move | null {
         const move = game.value.cancelLastMove();
         if (move) {
-            chessboard.value = game.value.getChessboard();
+            board.value.undoMove(move);
+            squares.value = board.value.getSquares();
             legalMoves.value = game.value.getLegalMoves();
+            lastHalfmoveIndex.value--;
+            activeHalfmoveIndex.value--;
         }
         return move;
     }
@@ -45,7 +103,7 @@ export function useLocalChess() {
     return {
         rows,
         columns,
-        chessboard,
+        squares,
         legalMoves,
         playerInFrontIndex,
         playerInFrontDirection,
@@ -53,6 +111,10 @@ export function useLocalChess() {
         isLegalMove,
         tryMove,
         spinChessboard,
+        goToFirstMove,
+        goToPreviousMove,
+        goToNextMove,
+        goToLastMove,
         cancelLastMove,
     };
 }
