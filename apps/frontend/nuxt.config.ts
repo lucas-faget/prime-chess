c250693afc0d4d3b1736aaf4c0d148da87ac54f4
lucas-faget <lucasfaget98@gmail.com>
2025-11-10 19:38:48 +0100

    build(apps/frontend): add primevue

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,6 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+import Aura from "@primeuix/themes/aura";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss"],
+    css: ["./app/app.css"],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    modules: ["@primevue/nuxt-module"],
+    primevue: {
+        options: {
+            theme: {
+                preset: Aura,
+            },
+        },
+    },
 });
